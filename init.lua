4f75aa50a3eebacc97d6fd3fcaec7a92395f8ec1
1337est <59178218+1337est@users.noreply.github.com>
2025-11-11 17:25:23 -0600

    Added lualine

--- init.lua
+++ init.lua
@@ -208,43 +208,35 @@ autocmd({ "BufEnter" }, {
 -- TODO: Add require's pack/plugins/start/plugin/ for others not listed here.
 require("colorizer").setup()
 require("ibl").setup()
+local symbols = { error = ' ', warn = ' ', info = ' ', hint = ' ' }
+require("lualine").setup {
+    -- options = {
+    --     theme = "auto",
+    --     disabled_filetypes = {
+    --         statusline = { 'NvimTree' },
+    --         winbar = { 'NvimTree' },
+    --     },
+    -- },
+    -- sections = {
+    --     lualine_a = { { "mode" } },
+    --     lualine_b = { { "diagnostics", symbols = symbols, color = { bg = '#232323' } } },
+    --     lualine_c = { { "filename", path = 4 } },
+    --     lualine_x = { {
+    --         "filetype",
+    --         icon = { align = 'left' }
+    --     } },
+    --     lualine_y = {
+    --         { "branch", color = { bg = '#232323' } },
+    --         { "diff", color = { bg = '#323232' } },
+    --     },
+    --     lualine_z = { "location", "progress" },
+    -- },
+    -- inactive_sections = {
+    --     lualine_a = { "filename" },
+    --     lualine_z = { "location", "progress" },
+    -- },
+}
 
---         {
---             "nvim-lualine/lualine.nvim",
---             enabled = true,
---             dependencies = { "nvim-tree/nvim-web-devicons" },
---             config = function()
---                 local symbols = { error = ' ', warn = ' ', info = ' ', hint = ' ' }
---                 require("lualine").setup {
---                     options = {
---                         theme = "auto",
---                         disabled_filetypes = {
---                             statusline = { 'NvimTree' },
---                             winbar = { 'NvimTree' },
---                         },
---                     },
---                     sections = {
---                         lualine_a = { { "mode" } },
---                         lualine_b = { { "diagnostics", symbols = symbols, color = { bg = '#232323' } } },
---                         lualine_c = { { "filename", path = 4 } },
---                         lualine_x = { {
---                             "filetype",
---                             icon = { align = 'left' }
---                         } },
---                         lualine_y = {
---                             { "branch", color = { bg = '#232323' } },
---                             { "diff", color = { bg = '#323232' } },
---                         },
---                         lualine_z = { "location", "progress" },
---                     },
---                     inactive_sections = {
---                         lualine_a = { "filename" },
---                         lualine_z = { "location", "progress" },
---                     },
---                 }
---             end,
---         },
---
 --         {
 --             'romgrk/barbar.nvim',
 --             dependencies = {
