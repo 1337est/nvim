697e9527e0eac366cfabad3650f7a9fb1ecb0295
1337est <59178218+1337est@users.noreply.github.com>
2025-11-26 14:53:49 -0600

    Making this my own. Next step to move away from pack dir, and move to plugin dir

---
+++ pack/plugins/start/nvim-web-devicons/plugin/nvim-web-devicons.lua
@@ -0,0 +1,8 @@
+-- Load guard: if we've already loaded, get the heck out of there!
+if vim.g._1337_icons_loaded then
+  return
+end
+vim.g._1337_icons_loaded = 1 -- locked and loaded baby
+
+-- TODO: Find out what this does?
+vim.g._1337Icons = 1
